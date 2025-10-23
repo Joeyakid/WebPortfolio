489148295dd591a40fa6659eb78188b26b2dddde
YKBZUBAIR <Yakjoy6@gmail.com>
2025-10-23 13:04:10 +0100

    Update

--- src/Static/Project.jsx
+++ src/Static/Project.jsx
@@ -9,7 +9,19 @@ const projects = [
         image: "./zyra.png"
     },
     {
-        title: "Portfolio website Website",
+        title: "PiggyVestReplica",
+        description: "PiggyVestReplica built with react, typescript, tailwindcss for front end",
+        link: "https://piggy-vest-replica-owjg.vercel.app/",
+        image: "./piggyVest.png"
+    },
+    {
+        title: "Moove",
+        description: "Moove Replica built with html, javascript and css",
+        link: "https://moove-ass-proj.vercel.app/",
+        image: "./moove.png"
+    },
+    {
+        title: "Portfolio Website",
         description: "Built with react, javascript, tailwindcss for front end and nodeJs, expressJs, nodemailer for backend",
         link: "https://web-portfolio-eta-two.vercel.app/",
         image: "./portweb.png"
@@ -17,12 +29,14 @@ const projects = [
 ]
 
 export default function Project () {
+    const repeatProject = [...projects, ...projects]
+
   return (
     <section className='bg-gray-500 text-white py-10 px-6'>
         <h2 className='font-bold text-3xl md:text-5xl mb-7 text-center'>Projects done</h2>
-        <div className='grid md:grid-cols-2 lg:-cols-3 gap-9'>
-            {projects.map((project, index) => (
-            <div key={index} className='bg-gray-700 shadow-lg overflow-hidden rounded-2xl'>
+        <div className='flex gap-9 md:w-[2600px] w-[2000px] flex-shrink-0 animate-marquee group-hover:[animation-play-state:paused] whitespace-nowrap'>
+            {repeatProject.map((project, index) => (
+            <div key={index} className='bg-gray-700 shadow-lg overflow-hidden rounded-2xl '>
                 <img src={project.image} alt={project.title} className='w-full object-cover' />
                 <div className='p-4'>
                     <h3 className='text-xl font-semibold'>{project.title}</h3>
@@ -31,6 +45,7 @@ export default function Project () {
                 </div>
             </div>
             ))}
+
         </div>
 
         <a href="https://github.com/Joeyakid?tab=repositories" className='hover:text-red-700'><span className='flex items-center gap-2 mt-8'>See more <FaArrowRight /></span></a>
